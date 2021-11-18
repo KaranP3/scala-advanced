1813d4b0ba7029777f1c4b860f604cd5084a216c
KaranP3 <karanpillai93@gmail.com>
2021-11-18 06:31:00 +0530

    add intro to JVM concurrency

--- src/lectures/part3concurrency/Intro.scala
+++ src/lectures/part3concurrency/Intro.scala
@@ -1,3 +1,52 @@
 package lectures.part3concurrency
 
-class Intro extends App {}
+import java.util.concurrent.Executors
+
+object Intro extends App {
+  /*
+  interface Runnable {
+    public void run()
+  }
+   */
+  // JVM threads
+  val runnable = new Runnable {
+    override def run(): Unit = println("Running in parallel")
+  }
+
+  val aThread = new Thread(runnable)
+
+  aThread.start() // gives the signal the the JVM to start a JVM thread
+  // create JVM thread => OS thread
+  runnable.run() //doesn't do anything in parallel
+  aThread.join() // blocks until a thread finishes running
+
+  val threadHello = new Thread(() => (1 to 5).foreach(_ => println("hello")))
+  val threadGoodbye = new Thread(
+    () => (1 to 5).foreach(_ => println("goodbye"))
+  )
+
+  threadHello.start()
+  threadGoodbye.start()
+  // different runs produce different results
+
+  // executors
+  val pool = Executors.newFixedThreadPool(10)
+  pool.execute(() => println("something in the thread pool"))
+
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("done after 1 second")
+  })
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("almost done")
+    Thread.sleep(1000)
+    println("done after 2 seconds")
+  })
+
+  pool.shutdown()
+  //  pool.execute(() => println("do not execute")) // throws an exception in the calling thread
+
+  //  pool.shutdownNow()
+  println(pool.isShutdown)
+}
